214041c580b83d38f8cb4fc2ea8e8a45eeaf9722
Gabriel Saratura <gabriel.saratura@vshn.ch>
2020-03-11 14:55:05 +0100

    Fix bugs

--- v3-nginx/docker-entrypoint.sh
+++ v3-nginx/docker-entrypoint.sh
@@ -7,12 +7,11 @@ ENV_VARIABLES=$(awk 'BEGIN{for(v in ENVIRON) print "$"v}')
 FILES=(
     etc/nginx/nginx.conf
     etc/nginx/conf.d/default.conf
-    etc/nginx/conf.d/tls.conf
+    etc/nginx/conf.d/logging.conf
     etc/modsecurity.d/modsecurity-override.conf
 )
 
 for FILE in ${FILES[*]}; do
-    echo $FILE
     if [ -f $FILE ]; then
         envsubst "$ENV_VARIABLES" <$FILE | sponge $FILE
     fi
